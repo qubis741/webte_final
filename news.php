79e7fc24f36785e312eb8f1f20156e3c8bac2720
Marek <marek14055@gmail.com>
2016-05-28 21:18:30 +0200

    dsa

--- news.php
+++ news.php
@@ -51,6 +51,7 @@
 	
 	$query = "SELECT headline" . getContentText($page,'lang') . " headline, text" . getContentText($page,'lang') ." text, timestamp FROM news ORDER BY timestamp DESC";
 	echo $query;
+	echo $getContentText($page,'lang');
 	$result = @mysql_query($query);
 	if(!$result){
 	   echo('Error selecting news: ' . mysql_error());
