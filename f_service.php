593c0c1c4245bec26741b9381bdc8df1a21aefc3
marekaro <marekaro@gmail.com>
2016-05-30 18:52:42 +0200

    Pridane jsn-rpc, export do csv

---
+++ f_service.php
@@ -0,0 +1,80 @@
+<form method="POST">
+  Zadajte výraz:<br>
+  <input type="text" name="expression" value="x+3"><br>
+   Zadajte API kľúč:<br>
+  <input type="text" name="API" ><br>
+   Zadajte začiatočnú súradnicu:<br>
+  <input type="number" name="x1" value="0"><br>
+  Zadajte poslednú súradnicu:<br>
+  <input type="number" name="x2" value="20"><br>  
+  <input type="submit" name="Submitt"value="Vypočítaj hodnoty">
+  <input type="submit" name="Submittt"value="Vypočítaj deriváciu">
+</form>
+
+
+
+
+<?php 			
+		require_once 'json-rpc/jsonRPCClient.php';
+		$fnc = new jsonRPCClient('http://147.175.98.147/final_zaloha/json-rpc/server.php');	
+
+	
+	if (!empty($_POST['Submitt'])) {
+		$expression = $_POST['expression'];
+		$x1= $_POST['x1'];
+		$x2 = $_POST['x2'];
+		$api = $_POST['API'];
+		
+		$result = $fnc->pocitajJSON($api, $x1, $x2, $expression);
+			echo "Arr1: result is: ";
+	
+	}
+	
+	if (!empty($_POST['Submittt'])) {
+		$expression = $_POST['expression'];
+		$x1= $_POST['x1'];
+		$x2 = $_POST['x2'];
+		$api = $_POST['API'];
+		
+		$result = $fnc->pocitajJSONderivacia($api, $x1, $x2, $expression);
+			echo "Arr1: result is: ";
+	
+	}
+		
+	if(isset($result )) {
+					session_start();
+					$_SESSION['csv'] = $result;
+					print_r($result);
+					
+	}
+	
+	
+	if (!empty($_POST['Submit'])) {
+		
+		header('Content-Type: application/csv');
+		header('Content-Disposition: attachment; filename="export.csv";');
+		session_start();
+		$arr = $_SESSION['csv'];
+
+		$file = fopen('php://output',"w");
+
+		foreach ($arr as $line)
+		  {
+		  fputcsv($file,explode(' ',$line));
+		  }
+
+		fclose($file);
+		session_destroy();
+
+	} 
+		
+			
+?>
+
+
+
+
+<form method="POST">
+  Exportujte do CSV:<br>
+  <input type="submit" name="Submit"value="Export">
+</form>
---
+++ json-rpc/server.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once 'jsonRPCServer.php';
+
+require 'fnc.php';
+
+$fnc = new fnc();
+jsonRPCServer::handle($fnc)
+	or print 'no request';
+?>
