6c791334d423e9e6929d1a5ff4e550893363b118
Kubko <jakub.kovacikk@gmail.com>
2016-05-30 19:15:11 +0200

    Oprava

--- global.php
+++ global.php
@@ -117,19 +117,4 @@ function sendNewsToSubs(){
     }
 
 }
-?><?php
-if(isset($_GET['lang'])){
-    $_SESSION['lang'] = $_GET['lang'];
-}
-if(isset($_SESSION['lang']))
-    $lang = $_SESSION['lang'];
-else
-    $lang = 'sk';
-
-$langText = simplexml_load_file('./langs.xml');
-function getContentText($page,$element){
-    global $langText, $lang;
-    return $langText->$page->$element->$lang;
-}
-
 ?>
